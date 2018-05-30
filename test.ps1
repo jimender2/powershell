bf265d9842d658f161be480b4fe4da9d6cda82d2
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-05-30 14:35:17 -0400

    updates

--- test.ps1
+++ test.ps1
@@ -2,14 +2,87 @@ Import-Module ActiveDirectory
 
 
 $Domain='blackriver.k12.oh.us'
-$studentOrStaff = Read-Host -Prompt 'Is this for a student or staff member?'
-$firstName = Read-Host -Prompt 'Input the first name of the user'
-$lastName = Read-Host -Prompt 'Input the last name of the user'
-$id = Read-Host -Prompt 'Input the student id of the user'
 
-$fullName = '18jmeredith'
-Write-Host $fullName
-$securepassword=ConvertTo-SecureString -String $id -AsPlainText -Force
+$creatorusername = ""
+$creatorpassword = ConvertTo-SecureString "" -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $creatorusername, $creatorpassword
 
-New-ADUser -Server blrv3 -Credential blrv\blrv -Name $fullName -AccountPassword $securepassword -DisplayName $fullName -Enabled $true -Organization Students
 
+$me=$env:username
+$studentou="OU=Students,DC=blackriver,DC=k12,DC=oh,DC=us"
+$teacherou="OU=Teachers,DC=blackriver,DC=k12,DC=oh,DC=us"
+$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+$continue = 'yes'
+
+while ($continue -match 'yes'){
+	$date=Get-date -Format "dd-MMM-yy"
+	
+	
+	$studorstaf = Read-Host -Prompt 'Is this for a student or staff member?'
+	$studentOrStaff = $studorstaf.toLower()
+	
+	if ($studentOrStaff -eq 'staff') {
+		$firstName = Read-Host -Prompt 'Input the first name of the staff member'
+		$lastName = Read-Host -Prompt 'Input the last name of the user'
+		
+		$fullName = $firstName + $lastName
+		$username = $firstName.Substring(0,1) + $lastName
+
+		Write-Host $fullName
+		$securepassword=ConvertTo-SecureString -String "pirate1" -AsPlainText -Force
+		
+		New-ADUser -Server blrv3 -Credential $cred -Name $username -GivenName $firstName -Surname $lastName -AccountPassword $securepassword -DisplayName $fullName -Enabled $true -Description $description -Organization $staffou --ChangePasswordAtLogon $true
+		
+		$UserDN  = (Get-ADUser -Server blrv3 -Credential $cred -Identity $fullName)
+		
+		$valid = 'no'
+		while ($valid -eq 'no') {		
+			$getwhatou = Read-Host -Prompt 'What do they do? (Board Office, Bus Driver, Cafeteria, Custodian, Office Staff)'
+			$whatou = $getwhatou.ToLower()
+			if ($whatou -match 'board office'){
+				$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+				$valid = 'yes'
+			} elseif ($whatou -match 'bus driver') {
+				$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+				$valid = 'yes'
+			} elseif ($whatou -match 'cafeteria') {
+				$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+				$valid = 'yes'
+			} elseif ($whatou -match 'custodian') {
+				$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+				$valid = 'yes'
+			} elseif ($whatou -match 'office staff') {
+				$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+				$valid = 'yes'
+			} else {
+				Write-Host 'ERROR: Enter one of the following Board Office, Bus Driver, Cafeteria, Custodian, Office Staff'
+			}
+		}
+			
+		Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+		
+	} elseif ($studentOrStaff -eq 'student'){
+		$firstName = Read-Host -Prompt 'Input the first name of the student'
+		$lastName = Read-Host -Prompt 'Input the last name of the student'
+		$id = Read-Host -Prompt 'Input the student id of the user'
+		
+		$fullName = $firstName + $lastName
+		Write-Host $fullName
+		$securepassword=ConvertTo-SecureString -String $id -AsPlainText -Force
+		
+		New-ADUser -Server blrv3 -Credential $cred -Name $fullName -GivenName $firstName -Surname $lastName -AccountPassword $securepassword -DisplayName $fullName -Enabled $true -Description $description -Organization $studentou
+		
+		$UserDN  = (Get-ADUser -Server blrv3 -Credential $cred -Identity $fullName)
+		Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $studentou
+	} else {
+		Write-Host "ERROR: Please enter staff or student"
+	}
+	
+	$continue = Read-Host -Prompt 'Do you want to continue'
+
+}
