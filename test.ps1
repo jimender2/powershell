996786eeddf24d66c0935dd4f9c511e06a7fdca5
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-05-31 14:58:54 -0400

    update

--- test.ps1
+++ test.ps1
@@ -25,7 +25,7 @@ while ($continue -match 'yes'){
 		$firstName = Read-Host -Prompt 'Input the first name of the staff member'
 		$lastName = Read-Host -Prompt 'Input the last name of the user'
 		
-		$fullName = $firstName + $lastName
+		$fullName = $firstName + ' ' + $lastName
 		$username = $firstName.Substring(0,1) + $lastName
 
 		Write-Host $fullName
@@ -40,23 +40,23 @@ while ($continue -match 'yes'){
 			$getwhatou = Read-Host -Prompt 'What do they do? (Board Office, Bus Driver, Cafeteria, Custodian, Office Staff)'
 			$whatou = $getwhatou.ToLower()
 			if ($whatou -match 'board office'){
-				$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				$staffou="OU=Board Office,OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
 				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
 				$valid = 'yes'
 			} elseif ($whatou -match 'bus driver') {
-				$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				$staffou="OU=Bus Drivers,OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
 				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
 				$valid = 'yes'
 			} elseif ($whatou -match 'cafeteria') {
-				$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				$staffou="OU=Cafeteria,OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
 				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
 				$valid = 'yes'
 			} elseif ($whatou -match 'custodian') {
-				$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				$staffou="OU=Custodians,OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
 				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
 				$valid = 'yes'
 			} elseif ($whatou -match 'office staff') {
-				$staffou="OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				$staffou="OU=Office Staff,OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
 				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
 				$valid = 'yes'
 			} else {
@@ -64,21 +64,51 @@ while ($continue -match 'yes'){
 			}
 		}
 			
-		Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
 		
 	} elseif ($studentOrStaff -eq 'student'){
 		$firstName = Read-Host -Prompt 'Input the first name of the student'
 		$lastName = Read-Host -Prompt 'Input the last name of the student'
 		$id = Read-Host -Prompt 'Input the student id of the user'
 		
-		$fullName = $firstName + $lastName
+				$fullName = $firstName + ' ' + $lastName
+		$username = $firstName.Substring(0,1) + $lastName
+
 		Write-Host $fullName
-		$securepassword=ConvertTo-SecureString -String $id -AsPlainText -Force
+		$securepassword=ConvertTo-SecureString -String "pirate1" -AsPlainText -Force
 		
-		New-ADUser -Server blrv3 -Credential $cred -Name $fullName -GivenName $firstName -Surname $lastName -AccountPassword $securepassword -DisplayName $fullName -Enabled $true -Description $description -Organization $studentou
+		New-ADUser -Server blrv3 -Credential $cred -Name $username -GivenName $firstName -Surname $lastName -AccountPassword $securepassword -DisplayName $fullName -Enabled $true -Description $description -Organization $staffou --ChangePasswordAtLogon $true
 		
 		$UserDN  = (Get-ADUser -Server blrv3 -Credential $cred -Identity $fullName)
-		Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $studentou
+		
+		$valid = 'no'
+		while ($valid -eq 'no') {		
+			$getwhatou = Read-Host -Prompt 'What do they do? (Board Office, Bus Driver, Cafeteria, Custodian, Office Staff)'
+			$whatou = $getwhatou.ToLower()
+			if ($whatou -match '2019'){
+				$staffou="OU=Board Office,OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+				$valid = 'yes'
+			} elseif ($whatou -match '201') {
+				$staffou="OU=Bus Drivers,OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+				$valid = 'yes'
+			} elseif ($whatou -match 'cafeteria') {
+				$staffou="OU=Cafeteria,OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+				$valid = 'yes'
+			} elseif ($whatou -match 'custodian') {
+				$staffou="OU=Custodians,OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+				$valid = 'yes'
+			} elseif ($whatou -match 'office staff') {
+				$staffou="OU=Office Staff,OU=Staff,DC=blackriver,DC=k12,DC=oh,DC=us"
+				Move-ADObject -Server blrv3 -Credential $cred -Identity $UserDN  -TargetPath $staffou
+				$valid = 'yes'
+			} else {
+				Write-Host 'ERROR: Enter the graduation year'
+			}
+		}
+
 	} else {
 		Write-Host "ERROR: Please enter staff or student"
 	}
